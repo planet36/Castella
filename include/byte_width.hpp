b7d90044a810d789380a4d545e8626a8619ec17c
Steven Ward <planet36@users.noreply.github.com>
2025-06-24 15:10:18 -0400

    Add header files

---
+++ include/byte_width.hpp
@@ -0,0 +1,23 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: OSL-3.0
+
+/// Get the byte width of an unsigned integer
+/**
+\file
+\author Steven Ward
+*/
+
+#pragma once
+
+#include <bit>
+#include <concepts>
+
+constexpr unsigned int
+byte_width(const std::unsigned_integral auto x)
+{
+    // std::bit_width(0) returns 0, but we want it to be 1
+    if (x == 0)
+        return 1;
+    const unsigned int w = static_cast<unsigned int>(std::bit_width(x));
+    return (w / 8) + (w % 8 != 0);
+}

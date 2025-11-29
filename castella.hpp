04636c71c47f47a59b1e03c183883ce019f4ee44
Steven Ward <planet36@users.noreply.github.com>
2025-11-29 09:15:02 -0500

    Remove function aes_enc_nr

--- castella.hpp
+++ castella.hpp
@@ -63,18 +63,6 @@
 
 using uint8x16_t = __m128i;
 
-/// Perform \a Nr rounds of AES encryption on \a data with \a aes_round_key
-static uint8x16_t
-aes_enc_nr(uint8x16_t data, const uint8x16_t aes_round_key, const unsigned int Nr) noexcept
-{
-    // Nr times
-    for (std::remove_const_t<decltype(Nr)> r = 0; r < Nr; ++r)
-    {
-        data = _mm_aesenc_si128(data, aes_round_key);
-    }
-    return data;
-}
-
 /// Perform 1 round of AES encryption with \a round_key on \a data
 static inline uint8x16_t
 aes_enc(uint8x16_t data, const uint8x16_t round_key)
@@ -110,27 +98,6 @@ aes_enc_0_inv(uint8x16_t data)
 // {{{ ARM64
 
 #include <arm_neon.h>
-
-/// Perform \a Nr rounds of AES encryption on \a data with \a aes_round_key
-static uint8x16_t
-aes_enc_nr(uint8x16_t data, const uint8x16_t aes_round_key, const unsigned int Nr) noexcept
-{
-    // https://blog.michaelbrase.com/2018/05/08/emulating-x86-aes-intrinsics-on-armv8-a/
-
-    // 1 time
-    data = vaeseq_u8(data, uint8x16_t{});
-    data = vaesmcq_u8(data);
-
-    // Nr-1 times
-    for (std::remove_const_t<decltype(Nr)> r = 1; r < Nr; ++r)
-    {
-        data = vaeseq_u8(data, aes_round_key);
-        data = vaesmcq_u8(data);
-    }
-
-    data ^= aes_round_key;
-    return data;
-}
 
 /// Perform 1 round of AES encryption with \a round_key on \a data
 static inline uint8x16_t

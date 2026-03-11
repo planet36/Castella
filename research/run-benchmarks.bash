22ecc3e97e1de0eeee128ab23eaa88d1dddddf64
Steven Ward <planet36@users.noreply.github.com>
2026-03-11 10:06:18 -0400

    Add shell scripts

---
+++ research/run-benchmarks.bash
@@ -0,0 +1,41 @@
+#!/usr/bin/bash
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: MPL-2.0
+
+export LC_ALL=C
+
+# Use N-1 threads
+NUM_THREADS="$(nproc --ignore=1)"
+export NUM_THREADS
+
+# Should be an odd number for simpler median
+BENCHMARK_REPS=5
+
+OUTPUT_DIR=results
+DATETIME=$(date -u +'%Y%m%dT%H%M%S')
+
+mkdir --verbose --parents -- "$OUTPUT_DIR" || exit
+
+for PROGRAM in *-benchmark
+do
+    test -x "$PROGRAM" || continue
+
+    echo "# $PROGRAM"
+    echo
+
+    ./"$PROGRAM" \
+        --benchmark_enable_random_interleaving=true \
+        --benchmark_repetitions="$BENCHMARK_REPS" \
+        --benchmark_report_aggregates_only=true \
+        --benchmark_out_format=console \
+        --benchmark_out="${OUTPUT_DIR}/${PROGRAM}.${DATETIME}.txt" || exit
+
+    echo
+    echo "## Sorted and filtered results"
+    echo
+
+    sh process-benchmark-result.sh "${OUTPUT_DIR}/${PROGRAM}.${DATETIME}.txt"
+
+    echo "________________________________________________________________________________"
+    echo
+done
---
+++ research/run-research.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/bash
+# SPDX-FileCopyrightText: Steven Ward
+# SPDX-License-Identifier: MPL-2.0
+
+export LC_ALL=C
+
+echo "# Print Castella size info and params"
+echo
+./castella-print-info || exit
+echo "________________________________________________________________________________"
+echo
+
+echo "# Find the optimal Nr for Castella::utils::aes_enc_0"
+echo
+./aes_enc_0-num_rounds -n 500000 || exit
+echo "________________________________________________________________________________"
+echo
+
+echo "# For each state size, find the optimal Nr for Castella::permute"
+echo
+./permute-num_rounds -n 1000 || exit
+echo "________________________________________________________________________________"
+echo
+
+echo "# Verify that Castella::permute_inv is the inverse of Castella::permute"
+echo
+./permute_inv-verify -n 100000 || exit
+echo "________________________________________________________________________________"
+echo

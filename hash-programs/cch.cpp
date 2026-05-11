655701dbb5988073197e06c628e164c9bcda2f74
Steven Ward <planet36@users.noreply.github.com>
2026-05-11 09:44:43 -0400

    Add cch.cpp

---
+++ hash-programs/cch.cpp
@@ -0,0 +1,310 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: MPL-2.0
+
+#include "bytes_to_hex.hpp"
+#include "cch.hpp"
+#include "fd-utils.h"
+#include "fnv.hpp"
+#include "quote_shell_always.hpp"
+#include "unique_fd.hpp"
+
+#include <cstdlib>
+#include <err.h>
+#include <fcntl.h>
+#include <fmt/format.h>
+#include <functional>
+#include <getopt.h>
+#include <numeric>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <unistd.h>
+#include <vector>
+
+inline constexpr std::string_view program_author = "Steven Ward";
+inline constexpr std::string_view program_license = "MPL-2.0";
+inline constexpr std::string_view program_version = "2026-05-11";
+
+// {{{ default values for options
+inline constexpr unsigned int default_digest_size_bytes = 32;
+static_assert(default_digest_size_bytes <= compress_castella_hash<>::get_max_digest_size_bytes());
+// }}}
+
+// {{{ options
+unsigned int digest_size_bytes = default_digest_size_bytes;
+bool use_mmap = true;
+// }}}
+
+/// Print the version information.
+void print_version()
+{
+    fmt::println("{} {}", program_invocation_short_name, program_version);
+    fmt::println("License {}", program_license);
+    fmt::println("Written by {}", program_author);
+}
+
+/// Print the help message.
+void print_usage()
+{
+    fmt::println("Usage: {} [OPTION]... [FILE]...", program_invocation_short_name);
+    fmt::println("");
+
+    fmt::println("Compute the Compress-Castella hash (CCH).");
+
+    fmt::println("If FILE is absent, or when FILE is '-', read standard input.");
+    fmt::println("");
+
+    fmt::println("OPTIONS");
+    fmt::println("");
+
+    fmt::println("  -V, --version");
+    fmt::println("        Print the version information, then exit.");
+
+    fmt::println("  -h, --help");
+    fmt::println("        Print this message, then exit.");
+
+    fmt::println("  --no-mmap");
+    fmt::println("        Do not use memory mapping to read FILE.");
+
+    fmt::println("  --size=SIZE");
+    fmt::println("        Specify the output size (in bytes).");
+    fmt::println("        Typical values are: 32, 48, or 64.");
+    fmt::println("        SIZE is clamped to {} bytes.",
+            compress_castella_hash<>::get_max_digest_size_bytes());
+    fmt::println("        (default={})", default_digest_size_bytes);
+
+    fmt::println("");
+
+    fmt::println("The output format is a line for each FILE with the following information:");
+    fmt::println("    digest, spaces, quoted FILE");
+    fmt::println("");
+
+    fmt::println("CCH ALGORITHM DESCRIPTION");
+    fmt::println("");
+
+    fmt::println("Input data is absorbed into the internal state via a one-way compression function.");
+    fmt::println("The internal state is mixed by the Castella permutation function every {} bytes of input, ensuring full state diffusion.", compress_castella_hash<>::DEFAULT_MIX_RATE);
+    fmt::println("To finalize the hash, padding bytes are appended to the final block and absorbed into the internal state via the compression function.");
+    fmt::println("The Castella permutation function is then applied to the state to produce the digest.");
+    fmt::println("");
+
+    fmt::println("https://github.com/planet36/Castella");
+    fmt::println("https://en.wikipedia.org/wiki/One-way_compression_function");
+}
+
+/// Process the command line options.
+/**
+* \param argc the arg count
+* \param argv the arg vector
+*/
+void process_options(int argc, char* argv[])
+{
+    using namespace std::literals;
+
+    const char* short_options = "+Vh";
+
+    constexpr int OPTION_HASH_VERSION  = static_cast<int>(fnv1a_32("version" ));
+    constexpr int OPTION_HASH_HELP     = static_cast<int>(fnv1a_32("help"    ));
+    constexpr int OPTION_HASH_SIZE     = static_cast<int>(fnv1a_32("size"    ));
+    constexpr int OPTION_HASH_NO_MMAP  = static_cast<int>(fnv1a_32("no-mmap" ));
+
+    using long_option = option;
+
+    constexpr long_option long_options[] =
+    {
+        // const char*, int              , int*   , int
+        // name       , has_arg          , flag   , val
+        {"version"    , no_argument      , nullptr, OPTION_HASH_VERSION },
+        {"help"       , no_argument      , nullptr, OPTION_HASH_HELP    },
+        {"size"       , required_argument, nullptr, OPTION_HASH_SIZE    },
+        {"no-mmap"    , no_argument      , nullptr, OPTION_HASH_NO_MMAP },
+        {nullptr      , 0                , nullptr, 0                   },
+    };
+
+    int c = 0;
+    while ((c = getopt_long(argc, argv, short_options, long_options, nullptr)) != -1)
+    {
+        switch (c)
+        {
+        case 'V':
+        case OPTION_HASH_VERSION:
+            print_version();
+            std::exit(EXIT_SUCCESS);
+            break;
+
+        case 'h':
+        case OPTION_HASH_HELP:
+            print_usage();
+            std::exit(EXIT_SUCCESS);
+            break;
+
+        case OPTION_HASH_SIZE:
+            try
+            {
+                const auto tmp = std::stol(optarg);
+                digest_size_bytes = std::saturating_cast<decltype(digest_size_bytes)>(tmp);
+            }
+            catch (const std::invalid_argument& ex)
+            {
+                (void)std::fflush(stdout);
+                errx(EXIT_FAILURE, "invalid argument: %s: \"%s\"", ex.what(), optarg);
+            }
+            catch (const std::out_of_range& ex)
+            {
+                (void)std::fflush(stdout);
+                errx(EXIT_FAILURE, "out of range: %s: \"%s\"", ex.what(), optarg);
+            }
+            break;
+
+        case OPTION_HASH_NO_MMAP:
+            use_mmap = false;
+            break;
+
+        default:
+            std::exit(EXIT_FAILURE);
+        }
+    }
+}
+
+
+// https://git.savannah.gnu.org/gitweb/?p=gnulib.git;a=blob;f=lib/sha512-stream.c;hb=HEAD#l36
+// Gnulib uses 32768 for the buffer size
+#define BLOCKSIZE 32768
+#if BLOCKSIZE % 128 != 0
+# error "invalid BLOCKSIZE"
+#endif
+
+// helper
+/**
+* \retval true upon error
+* \retval false upon success
+*/
+bool
+process_file_read_fd(int fd, auto& hash_obj)
+{
+    std::vector<uint8_t> buf(BLOCKSIZE);
+
+    ssize_t num_bytes_read = 0;
+    // https://www.man7.org/linux/man-pages/man3/read.3p.html#RETURN_VALUE
+    // read(3p) returns either an error code or the number of bytes read
+    while ((num_bytes_read = ::read(fd, std::data(buf), std::size(buf))) > 0)
+    {
+        hash_obj.add(std::data(buf), num_bytes_read);
+    }
+
+    return num_bytes_read < 0;
+}
+
+
+#define SYSERR_PATH(PATH) \
+    std::system_error(std::make_error_code(std::errc{errno}), quote_shell_always(PATH));
+
+void
+process_file(const std::string& path, auto& hash_obj)
+{
+    const unique_fd fd{(path == "-") ? ::dup(STDIN_FILENO) : ::open(path.c_str(), O_RDONLY)};
+
+    if (!fd.ok())
+        throw SYSERR_PATH(path);
+
+    // lock file for reading
+    if (acq_read_lock_fd(fd.get()) < 0)
+        throw SYSERR_PATH(path);
+
+    if (is_seekable(fd.get()))
+    {
+        if (fadvise_sequential_noreuse(fd.get()))
+            throw SYSERR_PATH(path);
+    }
+
+    if (use_mmap)
+    {
+        const long file_size = get_file_size(fd.get());
+        if (file_size < 0)
+            throw SYSERR_PATH(path);
+
+        if (file_size < BLOCKSIZE)
+        {
+            if (process_file_read_fd(fd.get(), hash_obj))
+                throw SYSERR_PATH(path);
+            return;
+        }
+
+        const size_t mmap_size = get_mmap_size(file_size);
+
+        void* mmap_addr = ::mmap(nullptr, mmap_size, PROT_READ, MAP_PRIVATE, fd.get(), 0);
+
+        if (mmap_addr == MAP_FAILED)
+        {
+            // mmap may have failed because the file isn't memory-mappable.
+
+            if (process_file_read_fd(fd.get(), hash_obj))
+                throw SYSERR_PATH(path);
+            return;
+        }
+
+        if (madvise_sequential_willneed(mmap_addr, file_size))
+            throw SYSERR_PATH(path);
+
+        hash_obj.add(mmap_addr, file_size);
+
+        if (::munmap(mmap_addr, mmap_size) < 0)
+            throw SYSERR_PATH(path);
+    }
+    else
+    {
+        if (process_file_read_fd(fd.get(), hash_obj))
+            throw SYSERR_PATH(path);
+    }
+}
+
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
+{
+    using namespace std::literals;
+
+    int exit_status = EXIT_SUCCESS;
+
+    process_options(argc, argv);
+
+    std::vector<std::string> paths;
+
+    for (int i = optind; i < argc; ++i)
+    {
+        paths.push_back(argv[i]);
+    }
+
+    if (paths.empty())
+    {
+        paths.push_back("-"); // stdin
+    }
+
+    for (const auto& path : paths)
+    {
+        try
+        {
+            compress_castella_hash<> hash_obj;
+
+            process_file(path, hash_obj);
+
+            const auto digest_bytes = hash_obj.final_digest_bytes(digest_size_bytes);
+
+            fmt::println("{}  {}", bytes_to_hex(digest_bytes), path);
+        }
+        catch (const std::invalid_argument& ex)
+        {
+            (void)std::fflush(stdout);
+            warnx("invalid argument: %s", ex.what());
+            exit_status = EXIT_FAILURE;
+            break;
+        }
+        catch (const std::system_error& ex)
+        {
+            (void)std::fflush(stdout);
+            warnx("%s", ex.what());
+            exit_status = EXIT_FAILURE;
+        }
+    }
+
+    return exit_status;
+}

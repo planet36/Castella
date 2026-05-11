3fa54f2ec1fd1326c43ac4c31b042ac89beae434
Steven Ward <planet36@users.noreply.github.com>
2026-05-11 09:38:50 -0400

    Add header files

---
+++ hash-programs/fnv.hpp
@@ -0,0 +1,260 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: MPL-2.0
+
+/// constexpr FNV hash functions for \c std::string_view
+/**
+* \file
+* \author Steven Ward
+* \sa http://www.isthe.com/chongo/tech/comp/fnv/index.html
+* \sa https://en.wikipedia.org/wiki/Fowler%E2%80%93Noll%E2%80%93Vo_hash_function
+* \sa https://www.ietf.org/archive/id/draft-eastlake-fnv-02.txt
+*/
+
+#pragma once
+
+#include <cstdint>
+#include <iterator>
+#include <span>
+#include <string_view>
+
+namespace fnv_const_32
+{
+
+// https://www.wolframalpha.com/input?i=PrimeQ%5B16777619%5D
+inline constexpr uint32_t fnv_prime = UINT32_C(16'777'619); // = 2**24 + 2**8 + 0x93
+inline constexpr uint32_t fnv_offset_basis = UINT32_C(2'166'136'261);
+
+}
+
+namespace fnv_const_64
+{
+
+// https://www.wolframalpha.com/input?i=PrimeQ%5B1099511628211%5D
+inline constexpr uint64_t fnv_prime = UINT64_C(1'099'511'628'211); // = 2**40 + 2**8 + 0xB3
+inline constexpr uint64_t fnv_offset_basis = UINT64_C(14'695'981'039'346'656'037);
+
+}
+
+/// FNV-1 32-bit hash
+constexpr auto
+fnv1_32(const std::string_view s)
+{
+    using namespace fnv_const_32;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto c : s)
+    {
+        const auto octet = static_cast<uint8_t>(c);
+        result *= fnv_prime;
+        result ^= octet;
+    }
+
+    return result;
+}
+
+/// FNV-1a 32-bit hash
+constexpr auto
+fnv1a_32(const std::string_view s)
+{
+    using namespace fnv_const_32;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto c : s)
+    {
+        const auto octet = static_cast<uint8_t>(c);
+        result ^= octet;
+        result *= fnv_prime;
+    }
+
+    return result;
+}
+
+/// FNV-1 64-bit hash
+constexpr auto
+fnv1_64(const std::string_view s)
+{
+    using namespace fnv_const_64;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto c : s)
+    {
+        const auto octet = static_cast<uint8_t>(c);
+        result *= fnv_prime;
+        result ^= octet;
+    }
+
+    return result;
+}
+
+/// FNV-1a 64-bit hash
+constexpr auto
+fnv1a_64(const std::string_view s)
+{
+    using namespace fnv_const_64;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto c : s)
+    {
+        const auto octet = static_cast<uint8_t>(c);
+        result ^= octet;
+        result *= fnv_prime;
+    }
+
+    return result;
+}
+
+/// FNV-1 32-bit hash
+template <std::input_iterator Iter>
+requires (sizeof(std::iter_value_t<Iter>) == 1)
+constexpr auto
+fnv1_32(Iter first, const Iter last)
+{
+    using namespace fnv_const_32;
+
+    auto result = fnv_offset_basis;
+
+    while (first != last)
+    {
+        result *= fnv_prime;
+        result ^= static_cast<uint8_t>(*first);
+        ++first;
+    }
+
+    return result;
+}
+
+/// FNV-1a 32-bit hash
+template <std::input_iterator Iter>
+requires (sizeof(std::iter_value_t<Iter>) == 1)
+constexpr auto
+fnv1a_32(Iter first, const Iter last)
+{
+    using namespace fnv_const_32;
+
+    auto result = fnv_offset_basis;
+
+    while (first != last)
+    {
+        result ^= static_cast<uint8_t>(*first);
+        result *= fnv_prime;
+        ++first;
+    }
+
+    return result;
+}
+
+/// FNV-1 64-bit hash
+template <std::input_iterator Iter>
+requires (sizeof(std::iter_value_t<Iter>) == 1)
+constexpr auto
+fnv1_64(Iter first, const Iter last)
+{
+    using namespace fnv_const_64;
+
+    auto result = fnv_offset_basis;
+
+    while (first != last)
+    {
+        result *= fnv_prime;
+        result ^= static_cast<uint8_t>(*first);
+        ++first;
+    }
+
+    return result;
+}
+
+/// FNV-1a 64-bit hash
+template <std::input_iterator Iter>
+requires (sizeof(std::iter_value_t<Iter>) == 1)
+constexpr auto
+fnv1a_64(Iter first, const Iter last)
+{
+    using namespace fnv_const_64;
+
+    auto result = fnv_offset_basis;
+
+    while (first != last)
+    {
+        result ^= static_cast<uint8_t>(*first);
+        result *= fnv_prime;
+        ++first;
+    }
+
+    return result;
+}
+
+/// FNV-1 32-bit hash
+constexpr auto
+fnv1_32(const std::span<const std::byte> bytes)
+{
+    using namespace fnv_const_32;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto b : bytes)
+    {
+        const auto octet = static_cast<uint8_t>(b);
+        result *= fnv_prime;
+        result ^= octet;
+    }
+
+    return result;
+}
+
+/// FNV-1a 32-bit hash
+constexpr auto
+fnv1a_32(const std::span<const std::byte> bytes)
+{
+    using namespace fnv_const_32;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto b : bytes)
+    {
+        const auto octet = static_cast<uint8_t>(b);
+        result ^= octet;
+        result *= fnv_prime;
+    }
+
+    return result;
+}
+
+/// FNV-1 64-bit hash
+constexpr auto
+fnv1_64(const std::span<const std::byte> bytes)
+{
+    using namespace fnv_const_64;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto b : bytes)
+    {
+        const auto octet = static_cast<uint8_t>(b);
+        result *= fnv_prime;
+        result ^= octet;
+    }
+
+    return result;
+}
+
+/// FNV-1a 64-bit hash
+constexpr auto
+fnv1a_64(const std::span<const std::byte> bytes)
+{
+    using namespace fnv_const_64;
+
+    auto result = fnv_offset_basis;
+
+    for (const auto b : bytes)
+    {
+        const auto octet = static_cast<uint8_t>(b);
+        result ^= octet;
+        result *= fnv_prime;
+    }
+
+    return result;
+}
---
+++ hash-programs/quote_shell_always.hpp
@@ -0,0 +1,61 @@
+// SPDX-FileCopyrightText: Steven Ward
+// SPDX-License-Identifier: MPL-2.0
+
+/// Quote the string for a POSIX shell
+/**
+* \file
+* \author Steven Ward
+* \sa https://en.cppreference.com/w/cpp/io/manip/quoted
+* \sa https://www.gnu.org/software/bash/manual/bash.html#Single-Quotes
+*
+* Note: Only \c std::string is supported.
+*/
+
+#pragma once
+
+#include <cctype>
+#include <cstdint>
+#include <string>
+#include <string_view>
+
+/// Quote the string for a POSIX shell
+/**
+* \sa https://www.gnu.org/software/bash/manual/bash.html#Single-Quotes
+* <blockquote>
+Enclosing characters in single quotes (‘'’) preserves the literal value of each
+character within the quotes. A single quote may not occur between single
+quotes, even when preceded by a backslash.
+</blockquote>
+*/
+std::string
+quote_shell_always(const std::string& s)
+{
+    constexpr char BACKSLASH = '\\';
+    constexpr char SINGLE_QUOTE = '\'';
+
+    constexpr char delim = SINGLE_QUOTE;
+
+    std::string result;
+    result.reserve(s.size() + 2);
+
+    result += delim;
+
+    for (const auto c : s)
+    {
+        if (c == delim)
+        {
+            result += SINGLE_QUOTE;
+            result += BACKSLASH;
+            result += SINGLE_QUOTE;
+            result += SINGLE_QUOTE;
+        }
+        else
+        {
+            result += c;
+        }
+    }
+
+    result += delim;
+
+    return result;
+}

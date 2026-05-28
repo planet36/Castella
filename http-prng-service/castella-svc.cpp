7a524c5b7228615bf01e7ef1ac2b4067e49470ca
Steven Ward <planet36@users.noreply.github.com>
2026-05-28 11:50:39 -0400

    Use std::ssize where possible

--- http-prng-service/castella-svc.cpp
+++ http-prng-service/castella-svc.cpp
@@ -243,7 +243,7 @@ process_req_squeeze(const httplib::Request& req, httplib::Response& res)
         const auto digest = hash_obj->squeeze_bytes(num_bytes_to_squeeze);
 
         consec_bytes_sqzd = std::saturating_add(consec_bytes_sqzd,
-                static_cast<decltype(consec_bytes_sqzd)>(std::size(digest)));
+                static_cast<decltype(consec_bytes_sqzd)>(std::ssize(digest)));
 
         res.set_content(reinterpret_cast<const char*>(std::data(digest)), std::size(digest),
                         content_type);

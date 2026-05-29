e8122928b4f30792f123b0dfc71f499348e773f3
Steven Ward <planet36@users.noreply.github.com>
2026-05-29 13:17:13 -0400

    Add more test values for mix-rate benchmark

--- hash-programs/benchmark.cch.mix-rate.bash
+++ hash-programs/benchmark.cch.mix-rate.bash
@@ -17,5 +17,5 @@ mkdir --verbose --parents -- "$OUTPUT_DIR" || exit
 # Vary --mix-rate
 hyperfine --shell=none --time-unit millisecond --warmup=5 \
     --export-csv "${OUTPUT_DIR}/benchmark.cch.mix-rate.${DATETIME}.csv" \
-    --parameter-list MIX-RATE 256,512,1024,2048,4096,8192,16384,32768,65535 \
+    --parameter-list MIX-RATE 256,362,512,724,1024,1448,2048,2896,4096,5793,8192,11585,16384,23170,32768,46341,65535 \
     './cch --mix-rate={MIX-RATE} /tmp/test.txt'

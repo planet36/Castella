f5886d45e91a15b37cd452f07fc3867ff20d3e61
Steven Ward <planet36@users.noreply.github.com>
2026-06-10 10:06:20 -0400

    Move architecture-specific code to within a function instead of being a separate function

--- research/simd_equal.hpp
+++ research/simd_equal.hpp
@@ -11,48 +11,26 @@
 
 #include "simd_types.hpp"
 
-#if defined(__x86_64__)
-
-#include <immintrin.h>
-
 [[nodiscard]] static inline bool
 simd128_equal(const uint8x16_t a, const uint8x16_t b)
 {
+#if defined(__x86_64__)
     const uint8x16_t neq = _mm_xor_si128(a, b);
     return _mm_test_all_zeros(neq, neq);
+#elif defined(__aarch64__) && defined(__ARM_NEON)
+    return vmaxvq_u8(veorq_u8(a, b)) == 0;
+#endif
 }
-
-#if defined(__AVX2__)
 
 [[nodiscard]] static inline bool
 simd256_equal(const uint8x16x2_t a, const uint8x16x2_t b)
 {
+#if defined(__x86_64__) && defined(__AVX2__)
     const uint8x16x2_t neq = _mm256_xor_si256(a, b);
     return _mm256_testz_si256(neq, neq);
-}
-
-#endif
-
 #elif defined(__aarch64__) && defined(__ARM_NEON)
-
-#include <arm_neon.h>
-
-[[nodiscard]] static inline bool
-simd128_equal(const uint8x16_t a, const uint8x16_t b)
-{
-    return vmaxvq_u8(veorq_u8(a, b)) == 0;
-}
-
-[[nodiscard]] static inline bool
-simd256_equal(const uint8x16x2_t a, const uint8x16x2_t b)
-{
     const uint8x16_t neq0 = veorq_u8(a.val[0], b.val[0]);
     const uint8x16_t neq1 = veorq_u8(a.val[1], b.val[1]);
     return vmaxvq_u8(vorrq_u8(neq0, neq1)) == 0;
-}
-
-#else
-
-#error "Architecture not supported"
-
 #endif
+}

2f733ddc54f1a25418a2884311c5a4ecff4e212a
Steven Ward <planet36@users.noreply.github.com>
2026-06-18 16:52:53 -0400

    Fix warning: switch has 3 consecutive identical branches [bugprone-branch-clone]

--- include/castella-permute.hpp
+++ include/castella-permute.hpp
@@ -57,8 +57,8 @@ consteval int NUM_ROUNDS_MIN()
 {
     switch (N)
     {
-    case 2: return 2;
-    case 4: return 2;
+    case 2:
+    case 4:
     case 8: return 2;
     case 16: return 3;
     default: break;

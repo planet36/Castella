ef0bba3496eed77e4e26b2060b9fe9eaf47e1017
Steven Ward <planet36@users.noreply.github.com>
2026-06-23 23:33:59 -0400

    Shorten description of permute-num_rounds

--- research/run-research.sh
+++ research/run-research.sh
@@ -22,7 +22,7 @@ echo
 echo "________________________________________________________________________________"
 echo
 
-echo "# For each state size, find the minimum num_rounds for Castella::permute to achieve full bit diffusion"
+echo "# Find the minimum num_rounds for Castella::permute to achieve full bit diffusion"
 echo
 ./permute-num_rounds -n 120 || exit
 echo "________________________________________________________________________________"

b8c8907e82c5e72fdabb0c0d5d42079c753d78aa
Steven Ward <planet36@users.noreply.github.com>
2026-06-29 14:46:43 -0400

    Print Castella::AES_NUM_ROUNDS

--- research/castella-print-info.cpp
+++ research/castella-print-info.cpp
@@ -16,6 +16,8 @@ int main([[maybe_unused]] int argc, [[maybe_unused]] char* argv[])
     std::println("sizeof(uint8x16_t) = {}", sizeof(uint8x16_t));
     std::println("sizeof(Castella::block_t) = {}", sizeof(Castella::block_t));
 
+    std::println("Castella::AES_NUM_ROUNDS = {}", Castella::AES_NUM_ROUNDS);
+
     std::println("Castella::NUM_ROUNDS_MIN<2>() = {}", Castella::NUM_ROUNDS_MIN<2>());
     std::println("Castella::NUM_ROUNDS_MIN<4>() = {}", Castella::NUM_ROUNDS_MIN<4>());
     std::println("Castella::NUM_ROUNDS_MIN<8>() = {}", Castella::NUM_ROUNDS_MIN<8>());

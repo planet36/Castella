677af43d590a11c254eca8e30c74a591452d09fe
Steven Ward <planet36@users.noreply.github.com>
2026-06-30 00:06:16 -0400

    Rename HOST to BASE_URL

--- http-prng-service/test-client.sh
+++ http-prng-service/test-client.sh
@@ -2,14 +2,14 @@
 # SPDX-FileCopyrightText: Steven Ward
 # SPDX-License-Identifier: MPL-2.0
 
-HOST='http://localhost'
+BASE_URL='http://localhost'
 PORT=8080
 
 echo "# Send bytes to the service."
 
 echo
 echo "POST /absorb  # \"Lorem ipsum...\" (Content-Type: application/octet-stream)"
-cat <<'EOT' | curl --data-binary @- --header "Content-Type: application/octet-stream" "$HOST:$PORT/absorb" || exit
+cat <<'EOT' | curl --data-binary @- --header "Content-Type: application/octet-stream" "$BASE_URL:$PORT/absorb" || exit
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
 nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -23,7 +23,7 @@ sleep 4
 
 echo
 echo "POST /absorb  # nothing (a.k.a. \"blank\" call) (Content-Type: application/octet-stream)"
-printf '' | curl --data-binary @- --header "Content-Type: application/octet-stream" "$HOST:$PORT/absorb" || exit
+printf '' | curl --data-binary @- --header "Content-Type: application/octet-stream" "$BASE_URL:$PORT/absorb" || exit
 
 sleep 4
 
@@ -31,13 +31,13 @@ sleep 4
 # Specify Content-Type "application/octet-stream" to prevent this.
 echo
 echo "POST /absorb  # /dev/urandom (Content-Type: application/octet-stream)"
-head --bytes=32 /dev/urandom | curl --data-binary @- --header "Content-Type: application/octet-stream" "$HOST:$PORT/absorb" || exit
+head --bytes=32 /dev/urandom | curl --data-binary @- --header "Content-Type: application/octet-stream" "$BASE_URL:$PORT/absorb" || exit
 
 sleep 4
 
 echo
 echo "POST /absorb  # /dev/urandom (Content-Type: application/x-www-form-urlencoded)"
-head --bytes=32 /dev/urandom | curl --data-binary @- "$HOST:$PORT/absorb" || exit
+head --bytes=32 /dev/urandom | curl --data-binary @- "$BASE_URL:$PORT/absorb" || exit
 
 sleep 4
 
@@ -46,34 +46,34 @@ echo "# Receive bytes from the service."
 
 echo
 echo "GET /squeeze  # The default value is used."
-curl --show-error --silent "$HOST:$PORT/squeeze" | basenc --wrap=0 --base58 || exit ; echo
+curl --show-error --silent "$BASE_URL:$PORT/squeeze" | basenc --wrap=0 --base58 || exit ; echo
 
 sleep 4
 
 echo
 echo "GET /squeeze/  # The default value is used."
-curl --show-error --silent "$HOST:$PORT/squeeze/" | basenc --wrap=0 --base58 || exit ; echo
+curl --show-error --silent "$BASE_URL:$PORT/squeeze/" | basenc --wrap=0 --base58 || exit ; echo
 
 sleep 4
 
 echo
 echo "GET /squeeze/0  # nothing (a.k.a. \"mute\" call)"
-curl --show-error --silent "$HOST:$PORT/squeeze/0" | basenc --wrap=0 --base58 || exit ; echo
+curl --show-error --silent "$BASE_URL:$PORT/squeeze/0" | basenc --wrap=0 --base58 || exit ; echo
 
 sleep 4
 
 echo
 echo "GET /squeeze/32"
-curl --show-error --silent "$HOST:$PORT/squeeze/32" | basenc --wrap=0 --base58 || exit ; echo
+curl --show-error --silent "$BASE_URL:$PORT/squeeze/32" | basenc --wrap=0 --base58 || exit ; echo
 
 sleep 4
 
 echo
 echo "GET /squeeze/asdf  # The default value is used."
-curl --show-error --silent "$HOST:$PORT/squeeze/asdf" | basenc --wrap=0 --base58 || exit ; echo
+curl --show-error --silent "$BASE_URL:$PORT/squeeze/asdf" | basenc --wrap=0 --base58 || exit ; echo
 
 sleep 4
 
 echo
 echo "GET /squeeze/999999  # The value is clamped by Castella."
-curl --show-error --silent "$HOST:$PORT/squeeze/999999" | basenc --wrap=0 --base58 || exit ; echo
+curl --show-error --silent "$BASE_URL:$PORT/squeeze/999999" | basenc --wrap=0 --base58 || exit ; echo

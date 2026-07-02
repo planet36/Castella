d2c4c625b9446a1cece8120c728530bce14e2b64
Steven Ward <planet36@users.noreply.github.com>
2026-07-02 09:32:01 -0400

    Prefix round_key with "aes" when used in AES functions

--- include/aes_enc.hpp
+++ include/aes_enc.hpp
@@ -11,25 +11,25 @@
 
 #include "simd_types.hpp"
 
-/// Perform 1 round of AES encryption with \a round_key on \a data
+/// Perform 1 round of AES encryption with \a aes_round_key on \a data
 [[nodiscard]] static inline uint8x16_t
-aes_enc(uint8x16_t data, const uint8x16_t round_key) noexcept
+aes_enc(uint8x16_t data, const uint8x16_t aes_round_key) noexcept
 {
 #if defined(__x86_64__) && defined(__AES__)
-    return _mm_aesenc_si128(data, round_key);
+    return _mm_aesenc_si128(data, aes_round_key);
 #elif defined(__aarch64__) && defined(__ARM_FEATURE_AES)
-    return vaesmcq_u8(vaeseq_u8(data, uint8x16_t{})) ^ round_key;
+    return vaesmcq_u8(vaeseq_u8(data, uint8x16_t{})) ^ aes_round_key;
 #endif
 }
 
-/// Perform the inverse of 1 round of AES encryption with \a round_key on \a data
+/// Perform the inverse of 1 round of AES encryption with \a aes_round_key on \a data
 [[nodiscard]] static inline uint8x16_t
-aes_enc_inv(uint8x16_t data, const uint8x16_t round_key) noexcept
+aes_enc_inv(uint8x16_t data, const uint8x16_t aes_round_key) noexcept
 {
 #if defined(__x86_64__) && defined(__AES__)
-    return _mm_aesdeclast_si128(_mm_aesimc_si128(data ^ round_key), uint8x16_t{});
+    return _mm_aesdeclast_si128(_mm_aesimc_si128(data ^ aes_round_key), uint8x16_t{});
 #elif defined(__aarch64__) && defined(__ARM_FEATURE_AES)
-    return vaesdq_u8(vaesimcq_u8(data ^ round_key), uint8x16_t{});
+    return vaesdq_u8(vaesimcq_u8(data ^ aes_round_key), uint8x16_t{});
 #endif
 }
 
@@ -46,23 +46,23 @@ _mm256_aesimc_epi128(__m256i data) noexcept
     return _mm256_set_m128i(hi, lo);
 }
 
-/// Perform 1 round of AES encryption with \a round_key on \a data
+/// Perform 1 round of AES encryption with \a aes_round_key on \a data
 [[nodiscard]] static inline __m256i
-aes_enc(__m256i data, const __m256i round_key) noexcept
+aes_enc(__m256i data, const __m256i aes_round_key) noexcept
 {
-    return _mm256_aesenc_epi128(data, round_key);
+    return _mm256_aesenc_epi128(data, aes_round_key);
 }
 
-/// Perform the inverse of 1 round of AES encryption with \a round_key on \a data
+/// Perform the inverse of 1 round of AES encryption with \a aes_round_key on \a data
 [[nodiscard]] static inline __m256i
-aes_enc_inv(__m256i data, const __m256i round_key) noexcept
+aes_enc_inv(__m256i data, const __m256i aes_round_key) noexcept
 {
-    return _mm256_aesdeclast_epi128(_mm256_aesimc_epi128(data ^ round_key), __m256i{});
+    return _mm256_aesdeclast_epi128(_mm256_aesimc_epi128(data ^ aes_round_key), __m256i{});
 }
 
 #endif
 
-/// Perform 1 round of AES encryption with a zero round key on \a data
+/// Perform 1 round of AES encryption with a zero AES round key on \a data
 template <typename T>
 [[nodiscard]] static inline T
 aes_enc_0(T data) noexcept
@@ -70,7 +70,7 @@ aes_enc_0(T data) noexcept
     return aes_enc(data, T{});
 }
 
-/// Perform the inverse of 1 round of AES encryption with a zero round key on \a data
+/// Perform the inverse of 1 round of AES encryption with a zero AES round key on \a data
 template <typename T>
 [[nodiscard]] static inline T
 aes_enc_0_inv(T data) noexcept

e42b1f0defb6e41d962e025d26d15b5a6c923575
Steven Ward <planet36@users.noreply.github.com>
2026-07-03 15:20:57 -0400

    Rename input data file to have the size in its name

--- hash-programs/test-correctness.bash
+++ hash-programs/test-correctness.bash
@@ -125,7 +125,7 @@ function assert_neq_cmd_cmd
 }
 
 # Create the input data
-yes '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head --bytes 1M > /tmp/test.txt || exit
+yes '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz' | head --bytes 1M > /tmp/test-1MiB.txt || exit
 
 # `yes | head` raises SIGPIPE
 set -o pipefail
@@ -137,19 +137,19 @@ ROUNDS=3
 SUFFIX=0
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=16 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=16 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     a6c8f6cd0205c376e9f99e5ee013c965
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=32 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=32 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     3d9aa26606dc5f32a4857b8778c44e3d0131012710f81d6927677286b3b55fc3
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=48 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=48 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     d5ea5cf7b9270f2322bb008b21de32ef32f69c0551560989cf011422aadb1bb5a8e54b1107b67b106256abb962ab35b9
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=64 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS  --size=64 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     8c6284616ce8ecef11725e978d7916ee2e8a6db88301ac25e8b4d9a5d3693a2f57b8b65bc3803c54244b36652afac4d8538a913864f89c00738d63f43eed03ab
 
 CUSTOM='¡Ay, caramba!'
@@ -157,60 +157,60 @@ ROUNDS=16
 SUFFIX=105
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=16 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=16 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     96ee57b180faeefd58308bda0fadde95
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=32 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=32 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     6c6efbd552da0b144b37858755d2c1e81e310e5de4001ddeb1328c94e625ff35
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=48 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=48 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     fbfff200fcc8d8e58e5e86d9209d1a7635840c1cbc98aa95699eb8d67da7b5f4c9f36a8e0c67d93598c9950b5f197d12
 
 assert_eq_cmd_str \
-    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=64 --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
+    "./castella --custom='$CUSTOM' --rounds=$ROUNDS --size=64 --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
     b06b9329d823fa687b5fa9a1c5a2b0ad7b8da409c5ebd775f97c7c33121019c93e757356a21923f4b7318c6bd9945cffc1599c1bc1f1cc70f8af8d89e9ba1ab4
 
 assert_eq_cmd_str \
-    "./cch --size=16 /tmp/test.txt | cut -w -f 1" \
+    "./cch --size=16 /tmp/test-1MiB.txt | cut -w -f 1" \
     78d7ff6d3d3efe4d63c522c76700c2e7
 
 assert_eq_cmd_str \
-    "./cch --size=32 /tmp/test.txt | cut -w -f 1" \
+    "./cch --size=32 /tmp/test-1MiB.txt | cut -w -f 1" \
     78d7ff6d3d3efe4d63c522c76700c2e70e0ad3239eb266dfa7443132fd080653
 
 assert_eq_cmd_str \
-    "./cch --size=48 /tmp/test.txt | cut -w -f 1" \
+    "./cch --size=48 /tmp/test-1MiB.txt | cut -w -f 1" \
     78d7ff6d3d3efe4d63c522c76700c2e70e0ad3239eb266dfa7443132fd0806530da56ff302c4625eff6d433218a67b83
 
 assert_eq_cmd_str \
-    "./cch --size=64 /tmp/test.txt | cut -w -f 1" \
+    "./cch --size=64 /tmp/test-1MiB.txt | cut -w -f 1" \
     78d7ff6d3d3efe4d63c522c76700c2e70e0ad3239eb266dfa7443132fd0806530da56ff302c4625eff6d433218a67b8361ac6c7399638cdac8790478bcb17897
 
 # Verify that different "--custom" values give distinct results.
 
 assert_neq_cmd_cmd \
-    './castella --custom="Bart" /tmp/test.txt | cut -w -f 1' \
-    './castella --custom="Lisa" /tmp/test.txt | cut -w -f 1'
+    './castella --custom="Bart" /tmp/test-1MiB.txt | cut -w -f 1' \
+    './castella --custom="Lisa" /tmp/test-1MiB.txt | cut -w -f 1'
 
 # Verify that different "--rounds" values give distinct results.
 
 assert_neq_cmd_cmd \
-    './castella --rounds=4 /tmp/test.txt | cut -w -f 1' \
-    './castella --rounds=8 /tmp/test.txt | cut -w -f 1'
+    './castella --rounds=4 /tmp/test-1MiB.txt | cut -w -f 1' \
+    './castella --rounds=8 /tmp/test-1MiB.txt | cut -w -f 1'
 
 # Verify that different "--size" values give distinct results.
 
 assert_neq_cmd_cmd \
-    './castella --size=16 /tmp/test.txt | cut -w -f 1' \
-    './castella --size=32 /tmp/test.txt | cut -w -f 1'
+    './castella --size=16 /tmp/test-1MiB.txt | cut -w -f 1' \
+    './castella --size=32 /tmp/test-1MiB.txt | cut -w -f 1'
 
 # Verify that different "--suffix" values give distinct results.
 
 assert_neq_cmd_cmd \
-    './castella --suffix=105 /tmp/test.txt | cut -w -f 1' \
-    './castella --suffix=184 /tmp/test.txt | cut -w -f 1'
+    './castella --suffix=105 /tmp/test-1MiB.txt | cut -w -f 1' \
+    './castella --suffix=184 /tmp/test-1MiB.txt | cut -w -f 1'
 
 # Verify "--no-mmap" option produces the same output
 
@@ -221,32 +221,32 @@ do
     SUFFIX=0
 
     assert_eq_cmd_cmd \
-        "./castella --custom='$CUSTOM'           --rounds=$ROUNDS  --size=$SIZE --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
-        "./castella --custom='$CUSTOM' --no-mmap --rounds=$ROUNDS  --size=$SIZE --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1"
+        "./castella --custom='$CUSTOM'           --rounds=$ROUNDS  --size=$SIZE --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
+        "./castella --custom='$CUSTOM' --no-mmap --rounds=$ROUNDS  --size=$SIZE --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1"
 
     CUSTOM='¡Ay, caramba!'
     ROUNDS=16
     SUFFIX=105
 
     assert_eq_cmd_cmd \
-        "./castella --custom='$CUSTOM'           --rounds=$ROUNDS --size=$SIZE --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1" \
-        "./castella --custom='$CUSTOM' --no-mmap --rounds=$ROUNDS --size=$SIZE --suffix=$SUFFIX /tmp/test.txt | cut -w -f 1"
+        "./castella --custom='$CUSTOM'           --rounds=$ROUNDS --size=$SIZE --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1" \
+        "./castella --custom='$CUSTOM' --no-mmap --rounds=$ROUNDS --size=$SIZE --suffix=$SUFFIX /tmp/test-1MiB.txt | cut -w -f 1"
 
     assert_eq_cmd_cmd \
-        "./cch           --size=$SIZE /tmp/test.txt | cut -w -f 1" \
-        "./cch --no-mmap --size=$SIZE /tmp/test.txt | cut -w -f 1"
+        "./cch           --size=$SIZE /tmp/test-1MiB.txt | cut -w -f 1" \
+        "./cch --no-mmap --size=$SIZE /tmp/test-1MiB.txt | cut -w -f 1"
 done
 
 # Verify that sufficiently different "--mix-rate" values give distinct results.
 # The input file size must be at least 512 Bytes (twice the state size).
 
 assert_neq_cmd_cmd \
-    './cch --mix-rate=0 /tmp/test.txt | cut -w -f 1' \
-    './cch --mix-rate=1 /tmp/test.txt | cut -w -f 1'
+    './cch --mix-rate=0 /tmp/test-1MiB.txt | cut -w -f 1' \
+    './cch --mix-rate=1 /tmp/test-1MiB.txt | cut -w -f 1'
 
 assert_neq_cmd_cmd \
-    './cch --mix-rate=1 /tmp/test.txt | cut -w -f 1' \
-    './cch --mix-rate=2 /tmp/test.txt | cut -w -f 1'
+    './cch --mix-rate=1 /tmp/test-1MiB.txt | cut -w -f 1' \
+    './cch --mix-rate=2 /tmp/test-1MiB.txt | cut -w -f 1'
 
 echo "$PASS passed, $FAIL failed"
 

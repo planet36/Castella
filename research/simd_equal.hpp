748357baae68aaea9a15bb37734335710dccc66e
Steven Ward <planet36@users.noreply.github.com>
2026-07-12 19:09:43 -0400

    Make simd128_is_zero architecture-agnostic

--- research/simd_equal.hpp
+++ research/simd_equal.hpp
@@ -94,8 +94,12 @@ simd_arr_equal(const simd_arr_t<N>& lhs, const simd_arr_t<N>& rhs) noexcept
 [[nodiscard]] static inline bool
 simd128_is_zero(const uint8x16_t v) noexcept
 {
+#if defined(__SSE4_1__)
     // Returns 1 if (v & v) == 0
     return _mm_testz_si128(v, v) == 1;
+#elif defined(__aarch64__) && defined(__ARM_NEON)
+    return vmaxvq_u8(v) == 0;
+#endif
 }
 
 /// \copydoc simd128_is_zero(const uint8x16_t)

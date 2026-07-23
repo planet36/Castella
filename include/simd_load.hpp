6271da518df3f5c8dc6e15b5c569bf5fd26a5a15
Steven Ward <planet36@users.noreply.github.com>
2026-07-23 13:27:23 -0400

    Add DEBUG assert to simd_load16

--- include/simd_load.hpp
+++ include/simd_load.hpp
@@ -11,6 +11,9 @@
 
 #include "simd_types.hpp"
 
+#if defined(DEBUG)
+#include <cassert>
+#endif
 #include <cstddef>
 #include <cstring>
 #include <span>
@@ -34,6 +37,10 @@ simd_load16(const void* src) noexcept
 [[nodiscard]] static inline uint8x16_t
 simd_load16(const std::span<const std::byte> byte_sp) noexcept
 {
+#if defined(DEBUG)
+    assert(std::size(byte_sp) >= sizeof(uint8x16_t));
+#endif
+
     uint8x16_t dst{};
     (void)std::memcpy(&dst, std::data(byte_sp), sizeof(dst));
     return dst;

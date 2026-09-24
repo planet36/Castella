3f37fa15310f62c3cb70bfb69aa2a5644653ed3e
Steven Ward <planet36@users.noreply.github.com>
2026-09-24 17:49:50 -0400

    Remove some invocations of cch

--- hash-programs/benchmark.hash-programs.bash
+++ hash-programs/benchmark.hash-programs.bash
@@ -87,8 +87,6 @@ time hyperfine --shell=none --time-unit millisecond --warmup=5 \
 "./castella --chunk-size=262144 --rounds=3 --size=16 ${CASTELLA_TMP}/test.txt" \
 "${PIN}./cch --num-threads=1      ${CASTELLA_TMP}/test.txt" \
 "./cch                 ${CASTELLA_TMP}/test.txt" \
-"./cch --mix-rate=2048 ${CASTELLA_TMP}/test.txt" \
-"./cch --mix-rate=0    ${CASTELLA_TMP}/test.txt" \
 "./cch --chunk-size=262144 --mix-rate=0 ${CASTELLA_TMP}/test.txt" \
 "b3sum --tag                 ${CASTELLA_TMP}/test.txt" \
 "${PIN}b3sum --tag --no-mmap       ${CASTELLA_TMP}/test.txt" \
